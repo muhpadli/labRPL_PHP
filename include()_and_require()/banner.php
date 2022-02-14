10696ac60680576b57848afb255b6fde35961a93
muhpadli <muhp5934@gmail.com>
2022-02-14 18:47:13 +0700

    dasar2 PHP

---
+++ include()_and_require()/banner.php
@@ -0,0 +1,6 @@
+<div class="container-fluid banner pt-5 pb-5">
+    <div class="container text-center">
+        <h2> Selamat Datang di Website</h2>
+        <h2> Belajar Dasar PHP</h2>
+    </div>
+</div>
